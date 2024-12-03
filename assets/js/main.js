79e842b172d364d64534b3d9c4461a1638d848d9
Miguel <miviro0804@gmail.com>
2024-12-03 00:42:02 +0000

    push states

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,8 @@
 // assets/js/main.js
 import htmx from 'htmx.org';
 import Alpine from 'alpinejs';
-import markdownit from 'markdown-it';
 
 window.htmx = htmx;
 window.Alpine = Alpine;
-window.markdownit = markdownit;
 
 Alpine.start();
--- main.go
+++ main.go
@@ -8,19 +8,21 @@ import (
 	"html/template"
 )
 
-// Article represents a single article
-type Article struct {
-	Title string
-	Link  string
+type ArticleTemplateData struct {
+	ArticleName        string
+	ArticleTitle       string
+	ArticleDate        string
+	ArticleContent     string
+	ArticleReadingTime string
 }
 
-// PageData represents the data for the page
-type PageData struct {
-	PageTitle    string
-	SidebarTitle string
-	TextTitle    string
-	TextContent  string
-	Articles     []Article
+type IndexTemplateData struct {
+	PageTitle            string
+	SidebarTitle         string
+	TextTitle            string
+	TextContent          string
+	CurrentAddress       string
+	ArticleTemplateDatas []ArticleTemplateData
 }
 
 func main() {
@@ -41,20 +43,77 @@ func main() {
 	// Home route
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		// Dynamic data
-		data := PageData{
-			PageTitle:    "miviro",
-			SidebarTitle: "Articles",
-			TextTitle:    "Homepage",
-			TextContent:  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-			Articles: []Article{
-				{Title: "Article 1", Link: "/static/md/test.md"},
-				{Title: "Article 2", Link: "/article2"},
-				{Title: "Article 3", Link: "/article3"},
+		var currentAddress string
+		if r.URL.Path == "/" {
+			currentAddress = ""
+		} else {
+			currentAddress = r.URL.Path
+		}
+
+		data := IndexTemplateData{
+			PageTitle:      "miviro",
+			SidebarTitle:   "ArticleTemplateDatas",
+			TextTitle:      "Homepage",
+			TextContent:    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+			CurrentAddress: currentAddress,
+			ArticleTemplateDatas: []ArticleTemplateData{
+				{
+					ArticleName:        "go-introduction",
+					ArticleTitle:       "Introduction to Go",
+					ArticleDate:        "2023-10-01",
+					ArticleContent:     "Go is an open-source programming language created by Google.",
+					ArticleReadingTime: "5 min",
+				},
+				{
+					ArticleName:        "go-concurrency",
+					ArticleTitle:       "Concurrency in Go",
+					ArticleDate:        "2023-10-02",
+					ArticleContent:     "Go provides concurrency primitives as part of the core language.",
+					ArticleReadingTime: "10 min",
+				},
+				{
+					ArticleName:        "go-web",
+					ArticleTitle:       "Building Web Applications with Go",
+					ArticleDate:        "2023-10-03",
+					ArticleContent:     "Go is well-suited for building web applications due to its performance and simplicity.",
+					ArticleReadingTime: "8 min",
+				},
 			},
 		}
 
 		// Render the index.html template
 		if err := templates.ExecuteTemplate(w, "index.html", data); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+	})
+
+	// ArticleTemplateData route
+	http.HandleFunc("/articles/", func(w http.ResponseWriter, r *http.Request) {
+		// Extract the article name from the URL path
+		articleName := r.URL.Path[len("/articles/"):]
+
+		var data ArticleTemplateData
+		if articleName == "" {
+			data = ArticleTemplateData{
+				ArticleName:        "Home",
+				ArticleTitle:       "miviro.es",
+				ArticleDate:        "2023-10-01",
+				ArticleContent:     "Welcome to miviro.es",
+				ArticleReadingTime: "5 min",
+			}
+		} else {
+			// TODO: fetch .md article file and take values from it
+			// For now, we'll use a static example
+			data = ArticleTemplateData{
+				ArticleName:        articleName,
+				ArticleTitle:       articleName,
+				ArticleDate:        "2023-10-01",
+				ArticleContent:     "Go is an open-source programming language created by Google.",
+				ArticleReadingTime: "5 min",
+			}
+		}
+		// Render the article.html template
+		if err := templates.ExecuteTemplate(w, "article.html", data); err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 		}
 	})
