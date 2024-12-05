7fc58576001c829ca2cc5c3ae92b70a28ad2d2c1
Miguel <miviro0804@gmail.com>
2024-12-05 09:11:16 +0000

    particulas

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,55 @@
 // assets/js/main.js
 import htmx from 'htmx.org';
 import Alpine from 'alpinejs';
+import { tsParticles } from "@tsparticles/engine";
+import { loadSlim } from "@tsparticles/slim";
 
 window.htmx = htmx;
 window.Alpine = Alpine;
+
+async function loadParticles(options) {
+    await loadSlim(tsParticles);
+    await tsParticles.load({ id: "tsparticles", options });
+}
+
+const configs = {
+    particles: {
+        number: {
+            value: 100
+        },
+        color: {
+            value: "#ffffff"
+        },
+        links: {
+            enable: true,
+            distance: 200
+        },
+        shape: {
+            type: "circle"
+        },
+        opacity: {
+            value: 1
+        },
+        size: {
+            value: {
+                min: 4,
+                max: 6
+            }
+        },
+        move: {
+            enable: true,
+            speed: 0.5
+        }
+    },
+    background: {
+        color: "#f3f4f6"
+    },
+    poisson: {
+        enable: true
+    }
+};
+
+loadParticles(configs);
+
 
 Alpine.start();
